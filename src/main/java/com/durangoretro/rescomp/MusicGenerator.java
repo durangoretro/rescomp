78ca8b452c8042aa8c1e0dcc7a9a8a48e7a44e58
Emilio <emiliollbb@gmail.com>
2023-07-01 09:45:47 +0200

    Music XML conversion

--- src/main/java/com/durangoretro/rescomp/MusicGenerator.java
+++ src/main/java/com/durangoretro/rescomp/MusicGenerator.java
@@ -11,20 +11,25 @@ import org.dom4j.io.SAXReader;
 public class MusicGenerator {
 
 	public static byte[] convertToDurango(File file) throws Exception {
+		Integer divisions = 1;
 		SAXReader xmlReader = new SAXReader();
 		Document doc = xmlReader.read(file);
 		
-		Element part = doc.getRootElement().element("part");
-		Integer divisions = Integer.valueOf(part
-				.element("measure").element("attributes").element("divisions").getStringValue());
-		System.out.println("Divisions: " + divisions);
-		
-		Iterator<Element> notesIterator = part.element("measure").elementIterator("note");
-		while(notesIterator.hasNext()) {
-			Element note = notesIterator.next();
-			String step = note.element("pitch").element("step").getStringValue();
-			Integer duration = Integer.parseInt(note.element("duration").getStringValue());
-			System.out.println("NOTA: " + step + " -> "+ duration);
+		Iterator<Element> measures = doc.getRootElement().element("part").elementIterator("measure");
+		while(measures.hasNext()) {
+			Element measure = measures.next();
+			if(measure.element("attributes")!=null && measure.element("attributes").element("divisions")!=null) {
+				divisions = Integer.valueOf(measure.element("attributes").element("divisions").getStringValue());
+			}
+			System.out.println("Divisions: " + divisions);
+			
+			Iterator<Element> notesIterator = measure.elementIterator("note");
+			while(notesIterator.hasNext()) {
+				Element note = notesIterator.next();
+				String step = note.element("pitch").element("step").getStringValue();
+				Integer duration = Integer.parseInt(note.element("duration").getStringValue());
+				System.out.println("NOTA: " + step + " -> "+ duration);
+			}
 		}
 		
 		byte pixels[] = new byte[10];
